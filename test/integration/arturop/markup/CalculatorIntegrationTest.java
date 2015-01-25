164d054b8eb5422cd20a65463aed0c18ad30622b
Arturo Pie <arturop@nulogy.com>
2015-01-24 22:57:42 -0500

    adds integration tests for electronics material type

--- test/integration/arturop/markup/CalculatorIntegrationTest.java
+++ test/integration/arturop/markup/CalculatorIntegrationTest.java
@@ -23,4 +23,10 @@ public class CalculatorIntegrationTest {
 		double price = Calculator.computePrice(12456.95, 4, "books");
 		assertEquals(13707.63, price, 0.001);
 	}
+
+	@Test
+	public void testUsingElectronics() {
+		double price = Calculator.computePrice(54022.19, 17, Calculator.ELECTRONICS);
+		assertEquals(69429.32, price, 0.001);
+	}
 }
